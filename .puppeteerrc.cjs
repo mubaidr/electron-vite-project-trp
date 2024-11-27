6b50bbb5d442ee3a03264f9af7a781487beff0e8
Muhammad Ubaid Raza <mubaidr@gmail.com>
2024-11-27 17:23:34 +0500

    [wip] add build script to download and bundle puppeteer

--- .puppeteerrc.cjs
+++ .puppeteerrc.cjs
@@ -1,10 +1,10 @@
-// const { join } = require('node:path')
+const { join } = require('node:path')
 
 /**
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  skipDownload: false
-  // cacheDirectory: join(__dirname, '.electron-browsers'),
-  // we manage browsers via @puppeteer/browsers package
+  skipDownload: false,
+  cacheDirectory: join(__dirname, '.electron-browsers'),
+  // This is for dev only, we manage browsers via @puppeteer/browsers package for build time.
 }
--- scripts/setup.cjs
+++ scripts/setup.cjs
@@ -1,19 +1,11 @@
-const { install, BrowserPlatform, Browser, resolveBuildId } = require('@puppeteer/browsers')
+const { install, BrowserPlatform, Browser, resolveBuildId, getInstalledBrowsers } = require('@puppeteer/browsers')
 const { join } = require('path')
-
-// TODO: download binaries for all build targets in relative paths
 
 // Define the target directory for the Chromium binary
 const cacheDir = join(__dirname, '..', '.electron-browsers')
 
 // Create a function to download Chromium for all platforms
 async function downloadChromium() {
-  await install({
-    browser: Browser.CHROMEHEADLESSSHELL,
-    cacheDir,
-    buildId: await resolveBuildId(Browser.CHROMEHEADLESSSHELL, BrowserPlatform.LINUX, 'latest'),
-  })
-
   await install({
     browser: Browser.CHROMEHEADLESSSHELL,
     cacheDir,

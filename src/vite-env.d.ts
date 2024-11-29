e80094471b7a53b5e7833c73e5e6315080b006f3
Hussain <hello@ghulamhussain.dev>
2024-11-29 11:13:54 +0500

    conflict resolved

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,12 +2,6 @@
 
 interface Window {
   // expose in the `electron/preload/index.ts`
-<<<<<<< Updated upstream
   ipcRenderer: import("electron").IpcRenderer
-=======
-  ipcRenderer: import("electron").IpcRenderer;
-  puppeteer: {
-    getRankings: (domain: string, keywords: string[]) => Promise<string[]>;
-  };
->>>>>>> Stashed changes
+
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,3 @@
-<<<<<<< Updated upstream
-import { rmSync } from "node:fs"
-import path from "node:path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import electron from "vite-plugin-electron/simple"
-import pkg from "./package.json"
-
-// https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  rmSync("dist-electron", { recursive: true, force: true })
-
-  const isServe = command === "serve"
-  const isBuild = command === "build"
-  const sourcemap = isServe || !!process.env.VSCODE_DEBUG
-=======
 import { rmSync } from "node:fs";
 import path from "node:path";
 import { defineConfig } from "vite";
@@ -28,7 +12,6 @@ export default defineConfig(({ command }) => {
   const isServe = command === "serve";
   const isBuild = command === "build";
   const sourcemap = isServe || !!process.env.VSCODE_DEBUG;
->>>>>>> Stashed changes
 
   return {
     resolve: {
@@ -46,11 +29,7 @@ export default defineConfig(({ command }) => {
             if (process.env.VSCODE_DEBUG) {
               console.log(
                 /* For `.vscode/.debug.script.mjs` */ "[startup] Electron App"
-<<<<<<< Updated upstream
-              )
-=======
               );
->>>>>>> Stashed changes
             } else {
               args.startup();
             }
@@ -61,26 +40,18 @@ export default defineConfig(({ command }) => {
               minify: isBuild,
               outDir: "dist-electron/main",
               rollupOptions: {
-<<<<<<< Updated upstream
                 external: Object.keys(
                   "dependencies" in pkg ? pkg.dependencies : {}
                 ),
-=======
-                external: [
-                  ...Object.keys("dependencies" in pkg ? pkg.dependencies : {}),
-                ],
->>>>>>> Stashed changes
               },
             },
           },
         },
         preload: {
-          // Shortcut of `build.rollupOptions.input`.
-          // Preload scripts may contain Web assets, so use the `build.rollupOptions.input` instead `build.lib.entry`.
           input: "electron/preload/index.ts",
           vite: {
             build: {
-              sourcemap: sourcemap ? "inline" : undefined, // #332
+              sourcemap: sourcemap ? "inline" : undefined,
               minify: isBuild,
               outDir: "dist-electron/preload",
               rollupOptions: {
@@ -91,28 +62,17 @@ export default defineConfig(({ command }) => {
             },
           },
         },
-        // Ployfill the Electron and Node.js API for Renderer process.
-        // If you want use Node.js in Renderer process, the `nodeIntegration` needs to be enabled in the Main process.
-        // See 👉 https://github.com/electron-vite/vite-plugin-electron-renderer
         renderer: {},
       }),
     ],
     server:
       process.env.VSCODE_DEBUG &&
       (() => {
-<<<<<<< Updated upstream
-        const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL)
-        return {
-          host: url.hostname,
-          port: +url.port,
-        }
-=======
         const url = new URL(pkg.debug.env.VITE_DEV_SERVER_URL);
         return {
           host: url.hostname,
           port: +url.port,
         };
->>>>>>> Stashed changes
       })(),
     clearScreen: false,
   };
